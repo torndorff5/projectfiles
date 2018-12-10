84f4b14b4959946bb79258dca3260a5f6ec2a5ed
codeslinger <codeslinger@Kensleys-MBP.ad.uvu.edu>
2018-12-10 12:01:46 -0700

    Post Part II commit proj 4

--- C++/test/test/main.cpp
+++ C++/test/test/main.cpp
@@ -11,6 +11,20 @@ int fib(int n)
 
 
 int main () {
-    cout << fib(23) << endl;
-    
+    int a[30];
+    int cnt = 0;
+    int x = -1;
+    int y = 0;
+    while (x != 0){
+        cin >> x;
+        a[cnt++] = x;
+        y = fib(x);
+        a[cnt++] = y;
+        cout << "The fib of " << x << " is " << y << endl;
+    }
+    int i = 0;
+    cnt--;
+    while (cnt > i){
+        cout << a[i++]<< " " << a[cnt--] << " ";
+    }
 }
