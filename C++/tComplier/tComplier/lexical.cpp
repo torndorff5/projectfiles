63decf445d844d271c57eb8e985de5bb4bf40580
codeslinger <codeslinger@Kensleys-MBP.ad.uvu.edu>
2019-01-10 12:34:14 -0700

    Post numbers and spaces lexical analysis

--- C++/tComplier/tComplier/lexical.cpp
+++ C++/tComplier/tComplier/lexical.cpp
@@ -25,23 +25,37 @@ struct lexical {
     
     
     //type enumeration
-    enum Type {numb, charact, id, punct, keyw, symb, uk, eof};
+    enum Type {space, numb, charact, id, punct, keyw, symb, uk, eof};
+    
     //token class that has a lexeme, line# and type
     struct token {
         string lexeme;
         int line_num;
-        Type t;
+        Type type;
     };
+    
+    
     
     //checks type of token and sets its type value
     static void checkType(token &t){
         //Reg expressions
-        std::regex reg_num("[0-9]");
+        std::regex reg_space("[ ]");
+        std::regex reg_numb("[0-9]");
+        std::regex reg_charact("[^0-9]");
+        
         std::smatch match;
         
-        if (std::regex_search(t.lexeme,match, reg_num)){//istoken a number?
+        if (std::regex_search(t.lexeme,match, reg_space)){//is token a space
+            cout << "curr is space" << endl;//debug output
+            t.type = space;
+        }
+        else if (std::regex_search(t.lexeme,match, reg_numb)){//istoken a number?
             cout << "curr is number" << endl; //debug output
-            t.t = numb;
+            t.type = numb;
+        }
+        else if(std::regex_search(t.lexeme,match, reg_charact)){
+            cout << "curr is char" << endl; //debug output
+            t.type = charact;
         }
     }
     
@@ -64,10 +78,18 @@ struct lexical {
         }
         void nextToken(){
             //check to see if end of line
+            tanner orndorff : i was just about to write the code that would check for end of line and fetch another line into the buffer. 
             //if end of line, get next line.
             curr = next;
             getChar(buffer, next);//get char for lexeme
             checkType(next);//check type
+        }
+        void addnextToken(){
+            //test to see if end of line
+            //add lexeme of next to that of current and fetch the following char
+            curr.lexeme += next.lexeme;
+            getChar(buffer, next);
+            checkType(next);
         }
     };
     
@@ -77,6 +99,8 @@ struct lexical {
         //2.    Tokenize the input of the source file by grouping individual characters together to form tokens by scanning the input from LEFT-TO-RIGHT. Don't try to build a context into your scanner that is part of parsing this is just lexical analysis just work LEFT-TO-RIGHT.
         //3.    For each token created output the token type , line number and the lexeme associated with the token. This is debug code that you should make easy to turn off and on.
         //4.    Discard all comments until the end of a line is reached.
+        //vector of tokens
+        vector<token> tokens;
         fstream in;
         in.open(filename, ios::in);
         tokenface tf;
@@ -84,12 +108,23 @@ struct lexical {
             cout << "File opened successfully." << endl;//debug output
             std::getline(in,tf.buffer);//read in first line
             cout << tf.buffer << endl;//debug output
-            
             getChar(tf.buffer, tf.next);//place first char of first line into next
             checkType(tf.next);//check type of next token
-            tf.nextToken();//move to next token
-            tanner orndorff : i am about to test my code to see if it has successfully scanned the first two chars and set their token data to the right values. 
-
+            while(tf.curr.type != eof){
+                tf.nextToken();//move to next token, set current to next
+                if(tf.curr.type == space){//if the current token is a space
+                    //discard and go to next
+                    continue;
+                }
+                else if(tf.curr.type == numb){//if current is a number
+                    //check to see if next is a number
+                    while(tf.next.type == numb){
+                        //add next to current and fetch next
+                        tf.addnextToken();
+                    }
+                }
+                tokens.push_back(tf.curr);
+            }
             
         }
         else{
